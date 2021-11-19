4d53028f3bcfc1029be74999951c51b5d8baae9b
rotatorall <86522620+rotatorall@users.noreply.github.com>
2021-11-19 18:49:07 +0200

    Update sniff.php

--- sniff.php
+++ sniff.php
@@ -1,16 +1,29 @@
-<?php
-//Объявляем переменные даты, и переменные окружения
-$date=date("[D|d/m/Y|H:i]");
-$ip=getenv("Remote_addr");
-$real_ip=getenv("HTTP_X_FORWARDED_FOR");
-$otkuda=getenv("HTTP_REFERER");
-$browser=getenv("HTTP_USER_AGENT");
-$win=getenv("windir");
-$uid=implode($argv," ");
-//Открытие файла лога и запись в него данных,
+<? php
+ $snif[]= $_SERVER['HTTP_ACCEPT_CHARSET'];
+ $snif[]= $_SERVER['$HTTP_ACCEPT_LANGUAGE'];
+ $snif[]= $_SERVER['HTTP_CONNECTION'];
+ $snif[]= $_SERVER['HTTP_HOST'];
+ $snif[]= $_SERVER['HTTP_REFERER'];
+ $snif[]= $_SERVER['HTTP_USER_AGENT'];
+ $snif[]= $_SERVER['REMOTE_ADDR'];
+ $snif[]=$_SERVER['REMOTE_HOST'];
+ $snif[]=$_SERVER['REMOTE_PORT'];
+ $snif[]=$_SERVER['REQUEST_URI'];
+ $snif[]= $_SERVER['HTTP_COOKIE_VARS']; 
+ $snif[]= $_SERVER['COOKIE'];
+ $snif[]= $_SERVER['HTTP_X_FORWARDED_FOR'];
 
-$fp=fopen("log.txt","a");
-fputs($fp,"$date\t|$uid|\t$ip($real_ip)\t$browser\t$otkuda\t$win\n");
+$file = "file.txt"; 
+$s = fopen($file,"a+");
+$sa = fread ($s,filesize($file));
+$fp = fopen($file,"w");
+foreach ($snif as $sniffed)
+{
+fwrite($fp,"\n".$sniffed);
+}
+fwrite($fp,"\n");
+fwrite($s,$sa);
+fclose($s);
 fclose($fp);
-
-?>
+ 
+ ?>
